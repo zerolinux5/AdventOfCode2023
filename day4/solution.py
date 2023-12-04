d328ca8b8b1700a70a9bd780ee23b036feb7cd52
Jesus Magana <maganazero5@gmail.com>
2023-12-04 07:18:53 -0800

    Solved solution 2

--- day4/solution.py
+++ day4/solution.py
@@ -16,6 +16,39 @@ def soln1(lines):
         sum += runningSum
     return sum
 
+def countNumberOfCopies(winningNumbers, myNumbers):
+    count = 0
+    for number in myNumbers:
+        if number in winningNumbers:
+            count += 1
+    return count
+
+def soln2(lines):
+    cardCount = 0
+    cardMapping = {0: 0}
+    finalLength = len(lines)
+    for idx, line in enumerate(lines):
+        if idx in cardMapping:
+            cardMapping[idx] += 1
+        else:
+            cardMapping[idx] = 1
+        line = line.lower().strip()
+        _, cardInfo = line.split(':')
+        winningNumberCard, myNumberCard = cardInfo.split('|')
+        winningNumbers = set(winningNumberCard.split())
+        myNumbers = myNumberCard.split()
+        copyCount = countNumberOfCopies(winningNumbers, myNumbers)
+        for i in range(1, copyCount + 1):
+            newIdx = idx + i
+            if newIdx >= finalLength:
+                break
+            if newIdx in cardMapping:
+                cardMapping[newIdx] += cardMapping[idx]
+            else:
+                cardMapping[newIdx] = cardMapping[idx]
+    cardCount = sum(cardMapping.values())
+    return cardCount
+
 with open('input.txt') as f:
     lines = f.readlines()
-    print(soln1(lines))
+    print(soln2(lines))
